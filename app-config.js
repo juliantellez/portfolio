bdeaed1b830c96ecbc6ad6b905d0d0a477ba717a
Julian Tellez <juliantellezmendez@gmail.com>
2017-08-18 17:12:09 +0100

    adding eslint

---
+++ app-config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const APP_PORT = 8080
+
+const ENV = process.env.NODE_ENV || 'development'
+
+const SRC = path.join(__dirname, 'app')
+const SERVER = path.join(SRC, '/server')
+
+const DEST = path.join(__dirname, 'build')
+const STATIC = path.join(DEST, '/static')
+
+module.exports = {
+  APP_PORT,
+  ENV,
+  SRC,
+  SERVER,
+  DEST,
+  STATIC,
+}
---
+++ scripts/webpack/server.config.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+
+const config = require('../../app-config')
+
+const nodeModules = fs.readdirSync('node_modules')
+.reduce((modules, module) => {
+  if (module === '.bin') {
+    return modules
+  }
+  modules[module] = `commonjs ${module}`
+  return modules
+}, {})
+
+const webpackConfig = {
+  target: 'node',
+  entry: {
+    server: config.SERVER,
+  },
+  output: {
+    filename: '[name].js',
+    path: config.DEST,
+    sourceMapFileName: '[file].map',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: config.SRC,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['es2015', {modules: false}],
+              'stage-0',
+              'react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  externals: nodeModules,
+}
+
+exports.default = webpackConfig
