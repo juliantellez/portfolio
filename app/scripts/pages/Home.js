874f23e0d62f34a9cfa6742c5567eb89acf49502
Julian Tellez <juliantellezmendez@gmail.com>
2018-02-21 22:39:40 +0000

    working version

--- app/scripts/pages/Home.js
+++ app/scripts/pages/Home.js
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types'
 import Header from '../components/header/Header.component'
 import inject from '../components/utils/inject'
 import styles from './home/home.css'
+import canvasApp from './home/canvas'
 
 @inject('home$')
 class Home extends React.Component {
     static propTypes = {
       home$: PropTypes.object.isRequired,
     }
+    componentDidMount () {
+        canvasApp()
+    }
     render () {
       const {home$} = this.props
       return (
         <div className={styles.main}>
           <Header />
+            <canvas id="canvasOne" width="900" height="520" ></canvas>
           <div onClick={home$.onClick}>clik me</div>
         </div>
       )
